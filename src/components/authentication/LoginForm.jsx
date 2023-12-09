0146989a7b3b9fef816f8c7bf26a569608e1b99a
arif432 <nawazarif147@gmail.com>
2023-12-09 19:36:31 +0500

    stylings

--- src/components/authentication/LoginForm.jsx
+++ src/components/authentication/LoginForm.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
-
 export default function LoginForm() {
     const [email,setEmail] = useState()
     const [password,setPassword] = useState()
@@ -40,12 +39,16 @@ export default function LoginForm() {
             <form id="loginForm" className="space-y-4" onSubmit={handleSubmit}>
                 <div>
                     <label for="email" className="block mb-1" style={{color:"#333333"}}>Email:</label>
-                    <input type="email" id="email" name="email" className="w-full px-3 py-2 border border-gray-300 rounded"
+                    <input 
+                     style={styles.input}
+                    type="email" id="email" name="email" className="w-full px-3 py-2 border border-gray-300 rounded"
                     onChange={(E)=>setEmail(E.target.value)}/>
                 </div>
                 <div>
                     <label for="password" className="block mb-1" style={{color:"#333333"}}>Password:</label>
-                    <input type="password" id="password" name="password" className="w-full px-3 py-2 border border-gray-300 rounded"
+                    <input
+                    style={styles.input}
+                     type="password" id="password" name="password" className="w-full px-3 py-2 rounded"
                     onChange={(E)=>setPassword (E.target.value)}/>
                 </div>
                 <button type="submit" className="block w-full bg-blue-500 text-white py-2 rounded">Login</button>
@@ -56,3 +59,24 @@ export default function LoginForm() {
     </div>
   );
 }
+
+const styles = {
+    input: {
+        background: '#F0EEEE',
+        border: '1px solid #ccc',
+        padding: '8px',
+        borderRadius: '4px',
+    },
+    button: {
+        background: '#4285F4',
+        color: '#fff',
+        padding: '8px',
+        borderRadius: '4px',
+        width: '100%',
+    },
+    link: {
+        color: '#333',
+        textDecoration: 'underline',
+        textAlign: 'right',
+    },
+};
